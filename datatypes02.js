1b2cf59cb367660ed0692e29c8d7b8d611a3f913
Sridhar Das <111677489+Sridhar-2003@users.noreply.github.com>
2025-04-20 23:12:30 +0530

    Add files via upload

---
+++ datatypes02.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code as new version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+let name = "hitesh"
+let age = 25
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint - large numbers
+// string =>" "
+// boolean =>true/false
+// null => standalone value
+// undefined => variable is declared but not assigned a value
+// symbol => unique and immutable primitive values
+// object => collection of key value pairs
+// let person = {name: "hitesh", age: 25} //object
+// let person = ["hitesh", 25] //array
---
+++ variables01.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "das.113@gmail.com"
+var accountPassword = "0708"
+accountCity = "Odisha"
+
+accountEmail = "das.124@gmail.com"
+accountPassword = "22222"
+accountCity = "Manipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+/*
+ const-cannot be changed
+ let-used for variables
+ var-does not use due to issues in block scope and functional scope
+*/
